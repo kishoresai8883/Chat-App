7b2611a997e33ceb59ac4e1eb2dc11121cf7bc45
kishoresai <kishore.sai207@gmail.com>
2024-04-09 21:55:58 +0530

    Integrating Chat Routes

--- src/Components/Sidebar.js
+++ src/Components/Sidebar.js
@@ -1,4 +1,4 @@
-import React, {useState} from 'react'
+import React, { useState, useEffect, useContext } from 'react'
 import AccountCircleIcon from '@mui/icons-material/AccountCircle'
 import PersonAddIcon from '@mui/icons-material/PersonAdd'
 import GroupAddIcon from '@mui/icons-material/GroupAdd'
@@ -12,30 +12,38 @@ import ConversationsItem from './ConversationsItem'
 import { useNavigate } from 'react-router-dom'
 import { useDispatch, useSelector } from 'react-redux'
 import { toggleTheme } from '../Features/themeSlice'
-
+import axios from 'axios';
+import { refreshSidebarFun } from "../Features/refreshSidebar";
+import { myContext } from './MainContainer';
 import './Styles.css'
 
 const Sidebar = () => {
   const navigate = useNavigate();
   const dispatch = useDispatch();
   const lightTheme = useSelector((state) => state.themeKey);
-  const [conversations, setConversations] = useState([
-    {
-      name: "Test1",
-      lastMessage: "Last Msg #1",
-      timeStamp: "today",
-    },
-    {
-      name: "Jest2",
-      lastMessage: "Last Msg #2",
-      timeStamp: "today",
-    },
-    {
-      name: "Rest3",
-      lastMessage: "Last Msg #3",
-      timeStamp: "today",
-    },
-  ])
+  const {refresh, setRefresh} = useContext(myContext);
+  console.log("Context API : refresh : ", refresh);
+  const [conversations, setConversations] = useState([]);
+
+  const userData = JSON.parse(localStorage.getItem("userData"));
+  const nav = useNavigate();
+  if (!userData) {
+    console.log("User Not Authenticated");
+    nav("/");
+  }
+
+  const user = userData.data;
+  useEffect(() => {
+    const config = {
+      headers: {
+        Authorization: `Bearer ${user.token}`,
+      },
+    }
+    axios.get("http://localhost:8000/chat/", config).then((response) => {
+      console.log("Data Refresh in Sidebar ", response.data);
+      setConversations(response.data);
+    });
+  }, [refresh]);
   
   return (
     <div className='sidebar-container'>
@@ -83,8 +91,64 @@ const Sidebar = () => {
         <input placeholder='search' className={"search-input" + ((lightTheme) ? "" : " dark")}/>
       </div>
       <div className={"sb-conversations" + ((lightTheme) ? "" : " dark")}>
-        {conversations.map((conversation) => {
-          return <ConversationsItem props={conversation} key={conversation.name}/>
+        {conversations.map((conversation, index) => {
+          if(conversation.users.length === 1) {
+            return <div key={index}></div>
+          }
+          if(conversation.latestMessage === undefined) {
+            return (
+              <div key={index}
+                  onClick={() => {
+                    console.log("Refresh fired from Sidebar");
+                    setRefresh(!refresh);
+                  }}
+              >
+                <div key={index}
+                      className='conversation-container'
+                      onClick={() => {
+                        navigate("chat/" + conversation._id + "&" + conversation.users[1].name)
+                      }}
+                >
+                  <p className={"con-icon" + (lightTheme ? "" : " dark")}>
+                    {conversation.users[1].name[0]}
+                  </p>
+                  <p className={"con-title" + (lightTheme ? "" : " dark")}>
+                    {conversation.users[1].name}
+                  </p>
+
+                  <p className="con-lastMessage">
+                    No Messages here, Start a New Chat!!!
+                  </p>
+                </div>   
+              </div>
+            );
+          } else {
+            return (
+              <div
+                key={index}
+                className="conversation-container"
+                onClick={() => {
+                  navigate(
+                    "chat/" +
+                      conversation._id +
+                      "&" +
+                      conversation.users[1].name
+                  );
+                }}
+              >
+                <p className={"con-icon" + (lightTheme ? "" : " dark")}>
+                  {conversation.users[1].name[0]}
+                </p>
+                <p className={"con-title" + (lightTheme ? "" : " dark")}>
+                  {conversation.users[1].name}
+                </p>
+
+                <p className="con-lastMessage">
+                  {conversation.latestMessage.content}
+                </p>
+              </div>
+            );
+          }
         })}
       </div>
     </div>
